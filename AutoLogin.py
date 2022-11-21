c42653027f7a373b3139133994d42818f4f34493
jinhee <hanjinhee502@naver.com>
2022-11-21 18:20:23 +0900

    1. autoLogin 기능 추가 중. (비밀번호 암호화 포함)

---
+++ AutoLogin.py
@@ -0,0 +1,37 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import Qt
+from cryptography.fernet import Fernet
+
+class AutoLogin(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.autoLoginUI = uic.loadUi("./resources//autoLogin.ui")
+        self.securityKeyFile = open("./resources//securityKey.txt", "r")
+        self.setUp()
+
+    def setUp(self):
+        self.autoLoginUI.btn_saveLoginInfo.clicked.connect(self.btn_saveLoginInfoClicked)
+        self.securityKey = bytes(self.securityKeyFile.readline(), 'utf-8')  # Fernet.generate_key()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Enter:
+            self.saveEvent()
+
+    def btn_saveLoginInfoClicked(self):
+        self.saveEvent()
+
+    def encrypt(self, key):
+        fernet = Fernet(self.securityKey)
+        return fernet.encrypt(bytes(key, 'utf-8'))
+
+    def saveEvent(self):
+        ID = self.autoLoginUI.lineEdit_ID.text()
+        PW = self.autoLoginUI.lineEdit_PW.text()
+        self.saveInfo(ID, PW)
+
+    def saveInfo(self, ID, PW):
+        File = open("userInfo.txt", "w")
+        securityPW = self.encrypt(PW)
+        print(ID, file=File)
+        print(securityPW, file=File)
+        self.autoLoginUI.close()
--- mainWindow.py
+++ mainWindow.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets, uic
 from watermark import Watermark
 from commentCheck import CommentCheck
+from AutoLogin import AutoLogin
 import webbrowser
 
 class MainWindow(QtWidgets.QDialog):
@@ -13,6 +14,9 @@ class MainWindow(QtWidgets.QDialog):
     def setUp(self):
         self.ui.btn_watermark.clicked.connect(self.btn_watermarkClickedUI)
         self.ui.btn_commentCheck.clicked.connect(self.btn_commentCheckClickedUI)
+        self.ui.btn_visitNeighbor.clicked.connect(self.btn_visitNeighborClickedUI)
+        self.ui.actionmenu1.setText("자동 로그인 설정")
+        self.ui.actionmenu1.triggered.connect(self.autoLogin)
         self.ui.label_watermark.mousePressEvent = self.openWeb
 
     def btn_watermarkClickedUI(self):
@@ -25,6 +29,13 @@ class MainWindow(QtWidgets.QDialog):
         commentCheckUI.commentCheckUI.show()
         commentCheckUI.commentCheckUI.exec()
 
+    def btn_visitNeighborClickedUI(self):
+        print("Test")
+
+    def autoLogin(self):
+        autoLoginUI = AutoLogin()
+        autoLoginUI.autoLoginUI.show()
+        autoLoginUI.autoLoginUI.exec()
 
     def openWeb(self, event):
         webbrowser.open("https://blog.naver.com/hanjinhee502")
