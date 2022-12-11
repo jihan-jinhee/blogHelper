c13e48cdebdc005e867cd70cc7dd3b793fe53230
jinhee <hanjinhee502@naver.com>
2022-12-11 21:19:36 +0900

    1. 로그 날짜 폴더 정리

--- logWriter.py
+++ logWriter.py
@@ -5,8 +5,14 @@ import os
 if not os.path.exists("./log"):
     os.mkdir("log")
 
+logDate = time.strftime("%Y_%m%d")
+logTime = time.strftime("%Y_%m%d_%H%M")
+
+if not os.path.exists("./log//" + logDate):
+    os.mkdir("./log//" + logDate)
+
 Log_Format = "%(levelname)s %(asctime)s - %(message)s"
-filename = "./log//" + time.strftime("%Y_%m%d_%H%M") + "_logfile.log"
+filename = "./log//" + logDate + "//" + logTime + "_logfile.log"
 logging.basicConfig(filename=filename,
                     format=Log_Format,
                     level=logging.INFO)
